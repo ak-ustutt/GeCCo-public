52174f4cb7d202f289573dad2104f39db3160b69
Andreas Koehn <andreas@dhcppc1>
2009-03-25 12:19:59 +0100

    arghh ... forgotten files ...

---
+++ input/auto_freeze.f
@@ -0,0 +1,36 @@
+      subroutine auto_freeze(shell_def,nfreeze,orb_info)
+
+      implicit none
+
+      include 'stdunit.h'
+      include 'def_orbinf.h'
+
+      integer, intent(out) ::
+     &     shell_def(*)
+      integer, intent(in) ::
+     &     nfreeze
+      type(orbinf), intent(in), target ::
+     &     orb_info
+
+      integer, pointer ::
+     &     isym_bound_orbs(:)
+      integer ::
+     &     nfrz, idx, nsym
+
+      nfrz = nfreeze
+      if (nfreeze.lt.0) nfrz = orb_info%n_freeze_rcmd
+
+      if (nfrz.gt.orb_info%n_bound_orbs)
+     &     call quit(0,'auto_freeze',
+     &     'freezing more orbitals than possible!?!')
+
+      nsym = orb_info%nsym
+      isym_bound_orbs => orb_info%isym_bound_orbs
+      shell_def(1:nsym) = 0
+      do idx = 1, nfrz
+        shell_def(isym_bound_orbs(idx)) =
+     &       shell_def(isym_bound_orbs(idx))+1
+      end do
+
+      return
+      end
