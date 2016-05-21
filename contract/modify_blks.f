e01fdd774cc44c2d39ab6fe0ce45f8e27d7add82
Arne Bargholz <bargholz@theochem.uni-stuttgart.de>
2016-05-21 10:04:47 +0200

    refactored modify_blks

--- contract/modify_blks.f
+++ contract/modify_blks.f
@@ -6,9 +6,9 @@
 !!    @param[in] occ_descr string which defines the blocks to be set to val from
 !!    @param[in] val  use is dependend on mode
 !!    @param[in] mode string that defines the action
-!!                 "SET" 
-!!                 "SCALE"
-!!                 ""
+!!                 "SET"  sets all elements of the specified blocks to val
+!!                 "SCALE" scales all elements of the specified blocks by val
+!!                 "SHIFT" shifts all elements of the specified blocks to val
       subroutine modify_blks(mel,occ_descr,val,mode)
 *------------------------------------------------------------------------*
       implicit none
@@ -56,8 +56,8 @@
 
 !     variables for process_occ_descr
 !     maxdef: maximum number of possible occupation classes
-!     maxlist:  number of blocks
-!     nocc_cmp: number of created blocks(blocks or occ_cls?)
+!     maxlist:  maximum number of blocks
+!     nocc_cmp: number of created blocks (blocks or occ_cls?)
 !     occ_def_cmp: the ca_strings in touple form
         integer, parameter ::
      &     maxdef = 512
@@ -72,8 +72,7 @@
      &       njoined,len_op
 
 !     indices for the original operator
-!     idx_blk_* are starting and ending index of the relevant part in the buffer
-!     blk_* are starting and ending index of the relevant occupaiton classes
+!     idx_blk_* are starting and ending index of the relevant part in the file
 !     blk_no is the number current block; nblk ist the total number of blocks
         integer::
      &       idx_blk_nd,idx_blk_st,idx,
@@ -94,6 +93,8 @@
 
 
       iprint=max(iprlvl,ntest)
+
+
 
       if (iprint.ge.100) then
         call write_title(lulog,wst_dbg_subr,'modify_blocks')
@@ -124,133 +125,158 @@
         closeit = .false.
       end if
 
-!     maxnumber of records fitting into buffer
-      nblkmax = ifree/ffop%reclen
-
-
-      if (nblkmax.le.0) then
-        write(lulog,*) 'free memory (words):  ',ifree
-        write(lulog,*) 'block length (words): ',ffop%reclen
-        call quit(1,i_am,'not even 1 record fits into memory?')
-      end if
 
 !     offset on file (if more than one instance of operator exists(e.g.: multiple roots were calculated))
-      idisc_off = ffop%length_of_record*(ffop%current_record-1)
+!      idisc_off = ffop%length_of_record*(ffop%current_record-1)
 
 !     index-start right after the offset
-      idxst = idisc_off+1
+!     idxst = idisc_off+1
 
 
-        occ_def=>op%ihpvca_occ
-
-        njoined=op%njoined
+      occ_def=>op%ihpvca_occ
 
 
-        maxlist = maxdef/njoined
-
+      njoined=op%njoined
+      maxlist = maxdef/njoined
 !     Convert the string of occupation classes into an array 
-        call process_occ_descr(occ_def_cmp,nocc_cmp,
-     &       occ_descr,njoined,maxlist)
+      call process_occ_descr(occ_def_cmp,nocc_cmp,
+     &     occ_descr,njoined,maxlist)
 
  
 
       if (.not.ffop%buffered) then
 
-        len_op = mel%len_op
-
-        nblk = min((len_op-1)/ffop%reclen + 1,nblkmax)
-
-!     Bufferlength is either the full operator or a length that can hold the maximum amount of 
-!      records fitting into the free space
-        nbuff = min(len_op,nblk*ffop%reclen)
-
-        if (nbuff .lt. len_op) 
-     &       call quit(1,i_am,"not prepared ")
-
-        ifree = mem_alloc_real(buffer,nbuff,'buffer')
-        
-
-        buffer(1:nbuff) = 0d0
+!-----------------------------------------------------------------------
+!     calculate buffer length and allocate buffer
+         nbuff=lget_buf_len(ifree,mel%len_op,ffop%reclen)
+         ifree = mem_alloc_real(buffer,nbuff,'buffer')
+         buffer(1:nbuff) = 0d0
 
 
-!     taktik: 
 
-!     while idxst (starting index of block on file)
-!     is lower than end of operator:
-!     go on
-!        while (idxst.lt.idisc_off-1+len_op)
-
-!     idxnd=endindex on file.
-!     either offset+length of op 
-!     or (in case of iteration over blocks ) start of this block plus  
-        idxnd = min(idisc_off-1+len_op,
-     &               idxst-1+nbuff)
+!-----------------------------------------------------------------------
 
 
-! nocc number of full (all vertices) occupation classes
         nocc=op%n_occ_cls
-
-      if (iprint.ge.100) then
-        write(lulog,*) 'nbuff:  ',nbuff
-        write(lulog,*) 'idxst:  ',idxst
-        write(lulog,*) 'idxnd:  ',idxnd   
-        write(lulog,*) 'nocc:  ',nocc
-        write(lulog,*) 'nocc_cmp:  ',nocc_cmp   
-      end if
-        call get_vec(ffop,buffer,idxst,idxnd)
-!     Looping over all blocks of the MEL 
+        ! cycling over all occ_cls of MEL
         do iocc=1,nocc
-
-!     Looping over all blocks of the description
-           cmp_loop:do iocc_cmp=1,nocc_cmp
-
+           !cycling over all occ_cls of description
+           cmp_loop do iocc_cmp=1,nocc_cmp
+              ! if both blocks match:
               if (iocc_equal_n(
-     &             occ_def_cmp(1,1,(iocc_cmp-1)*njoined+1),.false.,
-     &             occ_def(1,1,(iocc-1)*njoined+1),.false.,njoined))
-     &             then
-                idx_blk_st=mel%off_op_occ(iocc)+1
-                idx_blk_nd=idx_blk_st+mel%len_op_occ(iocc)-1
-
-                if (iprint.ge.100) then
-                   write(lulog,*) "setting the following block to val"                   
-                   write(lulog,*) 'idx_blk_st:  ',idx_blk_st
-                   write(lulog,*) 'idx_blk_nd:  ',idx_blk_nd
-                   write(lulog,*) 'MODE:',mode
-                end if
-
-                select case(trim(mode))
-                case("SET")
-                   buffer(idx_blk_st:idx_blk_nd)=val
-                case("SHIFT")
-                   buffer(idx_blk_st:idx_blk_nd)=
-     &                  buffer(idx_blk_st:idx_blk_nd)+val
-                case("SCALE")
-                   buffer(idx_blk_st:idx_blk_nd)=
-     &                  buffer(idx_blk_st:idx_blk_nd)*val
-                case default
-                   call quit(1,i_am,'unknown mode')
-                end select
-                
-                exit cmp_loop ! assumes any occupation only occurs once in the 
-                              ! comparison loop
-                              ! so after match we can search for the next occupation
-              end if
+     &          occ_def_cmp(1,1,(iocc_cmp-1)*njoined+1),.false.,
+     &          occ_def(1,1,(iocc-1)*njoined+1),.false.,njoined))
+     &          then
+                 idx_blk_st=mel%off_op_occ(iocc)+1
+                 idx_blk_nd=idx_blk_st+mel%len_op_occ(iocc)-1
+                 idxst=idx_blk_st
+                 do while (idxst.lt.idx_blk_nd)
+!load either one buffer length or the full block/rest of block and modify it 
+                    idxnd = min(idxst-1+nbuff,idx_blk_nd)
+                    call get_vec(ffop,buffer,idxst,idxnd)
+                    call l_modify_buf(buffer,idxnd-idxst+1
+     &                   ,val,mode)
+                    call put_vec(ffop,buffer,idxst,idxnd)
+                    idxst=idxnd+1
+                 end do
+!assume every block exists only once on the description
+!( or doubly noted blocks should not be doubly modified)
+                 exit cmp_loop
+              endif
            end do cmp_loop
         end do
-        call put_vec(ffop,buffer,idxst,idxnd)
+
+
       else
          call  quit(1,i_am,'Not prepared for buffered vector')
       end if
-!      end while
-      call touch_file_rec(ffop)
 
+
+
+      call touch_file_rec(ffop)
       if (closeit)
      &     call file_close_keep(ffop)
 
       ifree = mem_flushmark()
       return
 
-      end
+      contains
+!-----------------------------------------------------------------------
+!> local subroutine that performs action on the buffer 
+!!
+!!
+!!       @param[inout] buffer the buffer to be changed     
+!!       @param[in] idxnd last element to be modified (first element is 1)
+!!       @param[in] val number to be modified by
+!!       @param[in] mode Modus see modify_blks    
+!-----------------------------------------------------------------------
+      subroutine l_modify_buf(buffer,idxnd,val,mode)
+!-----------------------------------------------------------------------
+      implicit none
+      character(len=25),parameter::
+     &     i_am="modify_blks:l_modify_buf"
+      character(len=*), intent(in) ::
+     &     mode
+      real(8),pointer, intent(inout):
+     &     buffer(*)
+      real(8), intent(in):
+     &     val
+      integer(8),intent(in):
+     &     idxnd
+
+      select case(trim(mode))
+      case("SET")
+         buffer(1:idxnd)=val
+      case("SHIFT")
+         buffer(1:idxnd)=
+     &        buffer(1:idxnd)+val
+      case("SCALE")
+         buffer(1:idxnd)=
+     &        buffer(1:idxnd)*val
+      case default
+         call quit(1,i_am,'unknown mode')
+      end select
+
+      end subroutine
+
+
+!-----------------------------------------------------------------------
+!>    local function to determine the buffer length
+!!
+!!
+!!    @param[in] ifree free memory 
+!!    @param[in] len_op total length of the operator  
+!!    @param[in] reclen length of a record
+!!    @return optimal length of the buffer
+!-----------------------------------------------------------------------  
+      function lget_buf_len(ifree,len_op,reclen) return(nbuff)
+!-----------------------------------------------------------------------
+      implicit none
+      include 'stdunit.h'
+      character(len=26),parameter::
+     &     i_am="modify_blks:l_get_buf_len"
+      integer,intent(in):: 
+     &     ifree,len_op,reclen
+      integer::
+     &     nblkmax,nblk
+      nblkmax = ifree/reclen
+      
+      if (nblkmax.le.0) then
+         write(lulog,*) 'free memory (words):  ',ifree
+         write(lulog,*) 'block length (words): ',reclen
+         call quit(1,i_am,'not even 1 record fits into memory?')
+      end if
+      
+!     Bufferlength is either the full operator or a length that can hold the maximum amount of 
+!     records fitting into the free space
+      nblk = min((len_op-1)/reclen + 1,nblkmax)
+      nbuff = min(len_op,nblk*reclen)
+
+      end function
+
+
+      end subroutine
+
 
 
 
