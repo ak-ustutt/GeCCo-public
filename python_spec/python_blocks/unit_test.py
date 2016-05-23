6d76a51cb620851384bc2eae6c31ffae64888684
Arne Bargholz <bargholz@theochem.uni-stuttgart.de>
2016-05-23 17:50:13 +0200

    reverted changes to modify_blks as it broke MRCC2-LR Added tests for modify blocks

--- python_spec/python_blocks/unit_test.py
+++ python_spec/python_blocks/unit_test.py
@@ -2,11 +2,12 @@ import re
 from gecco_interface import *
 
 
-tests=keywords.get('method.unit_test.tests')
-if re.search("MODIFY_MEL", tests) is not None:
+testing=keywords.get('method.unit_test.tests')
+
+if re.search("MODIFY_BLOCK", testing) is not None:
     import tests.mel
 
-if re.search("string_to_form", tests) is not None:
+if re.search("string_to_form", testing) is not None:
     import tests.stf
 
 
