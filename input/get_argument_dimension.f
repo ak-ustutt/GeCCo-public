9240ad7b741173cf1c9a86464ad2f2871bd5e3fe
Arne Bargholz <bargholz@theochem.uni-stuttgart.de>
2016-06-10 20:11:49 +0200

    adapted get_argument_dimension compiling NOT RUNNING

--- input/get_argument_dimension.f
+++ input/get_argument_dimension.f
@@ -9,8 +9,18 @@
 *     the first appearance in history is evaluated, unless count is set
 *----------------------------------------------------------------------*
 
-      use parse_input
+      use parse_input2, only : Node,NodeList,
+     &     input_doc, arg_tag,atr_name,atr_len,atr_kind,
+     &     getFirstChild, hasChildNodes, getChildNodes, getLength,
+     &     getNodeName, getAttribute, item, 
+     &     find_node
+      use FoX_common, only : rts
       implicit none
+
+      integer,parameter::
+     &     ntest=00
+      character(len=22),parameter ::
+     &     i_am="get_argument_dimension"
 
       integer, intent(out) ::
      &     dim
@@ -21,23 +31,29 @@
       integer, intent(out), optional ::
      &     type
 
-      type(keyword), pointer ::
-     &     curkey
-      type(argument), pointer ::
+      type(Node), pointer ::
+     &     curkey,input_root
+      type(Node), pointer ::
      &     curarg
+
+      type(NodeList), pointer ::
+     &     curargs
+
       character ::
      &     curcontext*1024
       integer ::
-     &     iargcount, icount_target, iargcount_target
+     &     iargcount, icount_target, iargcount_target, ii 
 
-      if (.not.associated(keyword_history%down_h))
-     &     call quit(1,'argument_dimension','invalid keyword history')
+
+      input_root=getFirstChild(input_doc)
+      if (.not.hasChildNodes(input_root))
+     &     call quit(1,i_am,'invalid keyword history')
 
       icount_target = 1
       if (present(keycount)) icount_target = keycount
 
-      call find_active_node(keyword_history,curkey,
-     &     context,icount_target)
+      call find_node(input_root,curkey,
+     &     context)
 
       dim = -1
       if (present(type)) type = -1
@@ -45,26 +61,23 @@
       iargcount_target = 1
       if (present(argcount)) iargcount_target = argcount
 
-      if (associated(curkey).and.associated(curkey%arg_h)) then
-        curarg => curkey%arg_h
+      if (associated(curkey).and.hasChildNodes(curkey)) then
+        curargs => getChildNodes(curkey)
+        arg_loop: do ii=1,getLength(curargs)
+          if (getNodeName(item(curargs, ii)) .eq. arg_tag) then 
+             if (trim(getAttribute(item(curargs,ii),atr_name))
+     &            .eq. trim(argkey) ) iargcount = iargcount+1
 
-        arg_loop: do 
-
-          if (trim(curarg%key).eq.trim(argkey)) iargcount = iargcount+1
-          if (iargcount.eq.iargcount_target) then
-            dim = curarg%val%len
-            if (present(type)) type = curarg%val%type
-            exit arg_loop
-          end if
-
-          if (associated(curarg%next)) then
-            curarg => curarg%next
-          else
-            exit arg_loop
-          end if        
-          
+             if (iargcount.eq.iargcount_target) then
+                call rts(getAttribute(item(curargs,ii), atr_len), dim)
+                if (present(type)) 
+     &               call rts( getAttribute(item(curargs,ii),atr_kind),
+     &               type)
+                exit arg_loop
+             end if
+          end if 
         end do arg_loop
-
+!> TODO convert length and kind into normalized attributes
       end if
 
       return
