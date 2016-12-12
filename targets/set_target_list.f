77ae83ceb625b74045f6d3c6b5ee7184de71e41d
Arne Bargholz <bargholz@theochem.uni-stuttgart.de>
2016-12-12 18:42:13 +0100

    chtf removing explicit setting unit tests in set_target_lists.f
             as now python_start is called unconditionally,
             the do all target was set twice

--- targets/set_target_list.f
+++ targets/set_target_list.f
@@ -78,12 +78,6 @@
      &     call set_mr_targets(tgt_info,orb_info,env_type,
      &     name_infile,fforbinf)
 
-      ! unit_tests
-      if (is_keyword_set('method.unit_test').gt.0)
-     &     call set_python_targets(tgt_info,
-     &     trim(gecco_path)//'/python_spec/python_start.py',
-     &     name_infile,fforbinf%name)
- 
 
 
       ! experimental section
