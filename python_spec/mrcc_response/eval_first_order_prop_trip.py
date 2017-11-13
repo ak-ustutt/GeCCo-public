9f468b7152e7865c0364a5b155c27e2d81b2e3d5
Andreas Koehn <koehn@theochem.uni-stuttgart.de>
2017-11-13 23:42:34 +0100

    Evaluate both singlet and triplet density (if triplet requested)

--- python_spec/mrcc_response/eval_first_order_prop_trip.py
+++ python_spec/mrcc_response/eval_first_order_prop_trip.py
@@ -8,17 +8,14 @@ _inp = GeCCo_Input()
 # Get the name of the package GeCCo uses the integrals from 
 _env = _inp.env
 
-if (_triplet):
-    absym_dens = -1.0
-else:
-    absym_dens = 1.0
-
 # Get the restart option to skip the calculation of lower order response properties
 _restart=_response_data['restart']
 
 ##########
 new_target('DENS_0')
-DEF_OP_FROM_OCC({LABEL:'DENS_0',JOIN:2,
+DEF_OP_FROM_OCC({LABEL:'DENS_0S',JOIN:2,
+                 DESCR:',;,|H,;,H|H,P;,|H,V;,|,;P,H|,P;P,|,V;P,|,;V,H|,P;V,|,V;V,'})
+DEF_OP_FROM_OCC({LABEL:'DENS_0T',JOIN:2,
                  DESCR:',;,|H,;,H|H,P;,|H,V;,|,;P,H|,P;P,|,V;P,|,;V,H|,P;V,|,V;V,'})
 ##########
 
@@ -52,9 +49,14 @@ INVARIANT({LABEL_RES:'F_LAG_DIP',
            OPERATORS:'H'})
 
 
-DERIVATIVE({LABEL_RES:'F_DENS_0',
+DERIVATIVE({LABEL_RES:'F_DENS_0S',
             LABEL_IN:'F_LAG_DIP',
-            OP_RES:'DENS_0',
+            OP_RES:'DENS_0S',
+            OP_DERIV:'DIPM'})
+
+DERIVATIVE({LABEL_RES:'F_DENS_0T',
+            LABEL_IN:'F_LAG_DIP',
+            OP_RES:'DENS_0T',
             OP_DERIV:'DIPM'})
 
 ##########
@@ -64,14 +66,17 @@ new_target('DEF_ME_DENS_0')
 
 depend('DENS_0')
 
-_op_list={'DENS_0':'ME_DENS_0'}
+_op_list={'DENS_0S':'ME_DENS_0S',
+          'DENS_0T':'ME_DENS_0T'}
+_ab_list={'DENS_0S':+1,
+          'DENS_0T':-1}
 
 for _op in _op_list:
     DEF_ME_LIST({LIST:_op_list[_op],
                  OPERATOR:_op,
                  IRREP:1,
                  '2MS':0,
-                 AB_SYM:absym_dens})
+                 AB_SYM:_ab_list[_op]})
 ##########
 
 ##########
@@ -83,7 +88,7 @@ depend('LIST_LMBD')
 depend('DEF_ME_C0','H0','DEF_ME_T')
 
 OPTIMIZE({LABEL_OPT:'FOPT_DENS_0',
-          LABELS_IN:'F_DENS_0'})
+          LABELS_IN:['F_DENS_0S','F_DENS_0T']})
 ##########
 
 ########## evaluating the density
@@ -104,11 +109,17 @@ else:
 depend('EVAL_DENS_0')
 
 _eval_prop_args={}
-_eval_prop_args[DENS]='ME_DENS_0'
 _eval_prop_args[RANK]=1
-if (_triplet):
-    _eval_prop_args[TRIPLET]=True
-else:
-    _eval_prop_args[TRIPLET]=False
+_eval_prop_args[DENS]='ME_DENS_0S'
+_eval_prop_args[TRIPLET]=False
 
+PRINT({STRING:'Evaluate properties from singlet density'})
+EVAL_PROP(_eval_prop_args)
+
+_eval_prop_args={}
+_eval_prop_args[RANK]=1
+_eval_prop_args[DENS]='ME_DENS_0T'
+_eval_prop_args[TRIPLET]=True
+
+PRINT({STRING:'Evaluate properties from triplet density'})
 EVAL_PROP(_eval_prop_args)
